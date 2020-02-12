ca413a71a9cde17999e872456036a8cb9d5eef7f
katos4 <gregoharriero@gmail.com>
2020-02-12 12:13:36 +0100

    prueba commit

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -9,7 +9,7 @@
                 <img src="http://localhost/tiendaLaravel/public/imagenes/logoVerde.png" width="120px" heigth="50px"/>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="{{ url('/') }}">Inic5io</a>
+                            <a class="nav-link" href="{{ url('/') }}">Inicio</a>
                             
                         </li>
                         <li class="nav-item dropdown">
